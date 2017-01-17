415ce9f776f7e8ce6d0c02a007c41edef2995394
dimargo08 <dimargo.081093@gmail.com>
2017-01-16 21:36:48 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package Principal;
+
+import Interfaz.Juego;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+            new Juego().setVisible(true);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
+
+}
